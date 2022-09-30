83ca6f938d8f4f1987173163da70c67dda3858f5
204G1A0552 <204G1A0552@srit.ac.in>
2022-09-30 16:06:41 +0530

    file is created

--- exp2/sjf.C
+++ exp2/sjf.C
@@ -1,10 +1,11 @@
+
 #include<stdio.h>
-#include<conio.h>
-main()
+
+int main()
 {
 int p[20], bt[20], wt[20], tat[20], i, k, n, temp; float wtavg,
 tatavg;
-clrscr();
+
 printf("\nEnter the number of processes -- ");
 
 scanf("%d", &n);
@@ -37,4 +38,5 @@ printf("\n\t PROCESS \tBURST TIME \t WAITING TIME\t TURNAROUND TIME\n");
 for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", p[i], bt[i], wt[i], tat[i]);
 printf("\nAverageWaiting Time -- %f", wtavg/n);
-printf("\nAverageTurnaround Time -- %f", tatavg/n); getch();}
+printf("\nAverageTurnaround Time -- %f", tatavg/n); }
+
